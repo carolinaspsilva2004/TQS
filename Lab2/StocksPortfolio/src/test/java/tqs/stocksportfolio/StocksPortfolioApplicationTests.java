117af1ddee547057d626eb7cb27c90c701692cf6
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-02-20 16:28:09 +0000

    criacao projeto ex1_lab2

---
+++ Lab2/StocksPortfolio/src/test/java/tqs/stocksportfolio/StocksPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.stocksportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksPortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
