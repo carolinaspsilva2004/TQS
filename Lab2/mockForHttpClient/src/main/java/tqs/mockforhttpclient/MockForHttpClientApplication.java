6ce8b572e31464488d6920a244e5fe73d14e8bc5
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-02-23 15:13:33 +0000

    projeto lab2_3

---
+++ Lab2/mockForHttpClient/src/main/java/tqs/mockforhttpclient/MockForHttpClientApplication.java
@@ -0,0 +1,13 @@
+package tqs.mockforhttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockForHttpClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockForHttpClientApplication.class, args);
+    }
+
+}
---
+++ Lab2/mockForHttpClient/src/test/java/tqs/mockforhttpclient/MockForHttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.mockforhttpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockForHttpClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
