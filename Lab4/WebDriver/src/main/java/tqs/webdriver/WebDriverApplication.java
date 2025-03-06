7729ba1253745727a2965ade8cd9931a7ac6fe7e
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-03-06 16:45:31 +0000

    lab4_1 projeto iniciado

---
+++ Lab4/WebDriver/src/main/java/tqs/webdriver/WebDriverApplication.java
@@ -0,0 +1,13 @@
+package tqs.webdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDriverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDriverApplication.class, args);
+    }
+
+}
