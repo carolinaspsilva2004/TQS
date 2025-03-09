d364f2189b22a17e2ec63e2cc430da052d677da2
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-03-09 17:32:37 +0000

    projeto lab4_3 iniciado

---
+++ Lab4/CoverBookStoreTest/src/main/java/tqs/coverbookstoretest/CoverBookStoreTestApplication.java
@@ -0,0 +1,13 @@
+package tqs.coverbookstoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverBookStoreTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoverBookStoreTestApplication.class, args);
+    }
+
+}
