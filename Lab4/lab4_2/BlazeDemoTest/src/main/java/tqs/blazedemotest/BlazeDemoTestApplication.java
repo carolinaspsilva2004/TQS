c76f0f30a9c0cd8389e0e5747f0758acabd89207
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-03-09 17:00:48 +0000

    projeto lab4_2

---
+++ Lab4/lab4_2/BlazeDemoTest/src/main/java/tqs/blazedemotest/BlazeDemoTestApplication.java
@@ -0,0 +1,13 @@
+package tqs.blazedemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlazeDemoTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlazeDemoTestApplication.class, args);
+    }
+
+}
