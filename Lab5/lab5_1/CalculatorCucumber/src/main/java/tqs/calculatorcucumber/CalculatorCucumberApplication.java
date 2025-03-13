2826c86536563cd97170985f607fc8571caa2195
Carolina Silva <119443000+carolinaspsilva2004@users.noreply.github.com>
2025-03-13 16:32:16 +0000

    projeto lab5_1 iniciado

---
+++ Lab5/lab5_1/CalculatorCucumber/src/main/java/tqs/calculatorcucumber/CalculatorCucumberApplication.java
@@ -0,0 +1,13 @@
+package tqs.calculatorcucumber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorCucumberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalculatorCucumberApplication.class, args);
+    }
+
+}
